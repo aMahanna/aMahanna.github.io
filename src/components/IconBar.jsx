14f9654859b8c88b4b47f88f201c1bc6dac31ab4
aMahanna <anthony.mahanna@gmail.com>
2021-07-15 16:35:31 -0400

    cleanup: images

--- src/components/IconBar.jsx
+++ src/components/IconBar.jsx
@@ -1,32 +1,15 @@
 import React from 'react';
 
-import Linkedin from '../img/icon-light/icons8-linkedin-50.png'
-import Github from '../img/icon-light/icons8-octocat-50.png'
-import Stackshare from '../img/icon-light/icons8-stackshare-50.png'
-
-import LinkedinDark from '../img/icon-dark/icons8-linkedin-50.png'
-import GithubDark from '../img/icon-dark/icons8-octocat-50.png'
-import StackshareDark from '../img/icon-dark/icons8-stackshare-50.png'
-
-
 import '../styles/iconbar.scss'
 
 export function IconBar(props) {
-    if (!props.darkMode) {
-        return (
-            <div className='social-bar'>
-                <a href="https://www.linkedin.com/in/amahanna/"><img src={Linkedin} /></a>
-                <a href="https://github.com/amahanna"><img src={Github} /></a>
-                <a href="https://stackshare.io/amahanna"><img src={Stackshare} /></a>
-            </div>
-        );
-    } else {
-        return (
-            <div className='social-bar'>
-                <a href="https://www.linkedin.com/in/amahanna/"><img src={LinkedinDark} /></a>
-                <a href="https://github.com/amahanna"><img src={GithubDark} /></a>
-                <a href="https://stackshare.io/amahanna"><img src={StackshareDark} /></a>
-            </div>
-        );
-    }
+    const colorPath = props.darkMode ? 'dark' : 'light';
+    return (
+        <div className='social-bar'>
+            <a href="https://www.linkedin.com/in/amahanna/"><img src={`./img/icons/linkedin.svg`} /></a>
+            <a href="https://github.com/amahanna"><img src={`./img/icons/github.svg`} /></a>
+            <a href="mailto:amaha100@uottawa.ca"><img src={`./img/icons/mail.svg`} /></a>
+            <a href="https://stackshare.io/amahanna"><img src={`./img/icons/stackshare.svg`} /></a>
+        </div>
+    );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 
-import MenuIconLight from "../img/icon-light/icons8-menu-vertical-50.png"
-import MenuIconDark from "../img/icon-dark/icons8-menu-vertical-50.png"
-
-import ModeDark from "../img/icon-dark/light-mode.png";
-import ModeLight from "../img/icon-light/light-mode.png";
-
 import "../styles/nav.scss";
 
 export default class Nav extends React.Component {
@@ -17,28 +11,12 @@ export default class Nav extends React.Component {
                         <div className="navbar-header">
                             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                                 <span className="sr-only">Toggle navigation</span>
-                                {
-                                    !this.props.darkMode &&
-                                    <img src={MenuIconLight} style={{ height: '20px' }} />
-                                }
-                                {
-                                    this.props.darkMode &&
-                                    <img src={MenuIconDark} style={{ height: '20px' }} />
-                                }
+                                <img src={`./img/icons/${this.props.darkMode ? 'dark' : 'light'}/menu.png`} style={{ height: '20px' }} />
                             </button>
                             <a className="navbar-brand font-grey" href="#">~</a>
-                            {
-                                this.props.darkMode &&
-                                    <a className="navbar-brand darkmode" onClick={this.props.toggleDarkMode}>
-                                        <img alt="Brand" src={ModeDark} style={{height: "100%" }} />
-                                    </a>
-                            }
-                            {
-                                !this.props.darkMode &&
-                                    <a className="navbar-brand darkmode" onClick={this.props.toggleDarkMode}>
-                                        <img alt="Brand" src={ModeLight} style={{height: "100%"}} />
-                                    </a>
-                            }
+                            <a className="navbar-brand darkmode" onClick={this.props.toggleDarkMode}>
+                                <img alt="Brand" src={`./img/icons/${this.props.darkMode ? 'dark' : 'light'}/mode.png`} style={{height: "100%" }} />
+                            </a>
                         </div>
                         <div id="navbar" className="collapse navbar-collapse navbar-right">
                             <ul className="nav navbar-nav">
